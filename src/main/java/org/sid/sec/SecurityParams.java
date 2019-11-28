e57d69b4f70563e0b4abaad09c1150fa70203de3
othmaneHadouani <36777370+othmaneHadouani@users.noreply.github.com>
2019-11-28 20:45:36 +0100

    Update SecurityParams.java

--- src/main/java/org/sid/sec/SecurityParams.java
+++ src/main/java/org/sid/sec/SecurityParams.java
@@ -2,7 +2,7 @@ package org.sid.sec;
 
 public interface SecurityParams {
     public static final String JWT_HEADER_NAME="Authorization";
-    public static final String SECRET="med@youssfi.net";
+    public static final String SECRET="hadouaniothmane@gmail.com";
     public static final long EXPIRATION=60000*60;
     public static final String HEADER_PREFIX="Bearer ";
 }
